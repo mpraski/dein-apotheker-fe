47333c4032519fcc73206c4b5bdde8fbeadb5d53
mpraski <marcin.praski@live.com>
2020-04-25 12:32:02 +0200

    Modify build process

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-source .gitlab-ci.env && gitlab-runner exec shell build --env CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE --env CI_REGISTRY=$CI_REGISTRY
+source .gitlab-ci.env && gitlab-runner exec shell build \
+    --env CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE \
+    --env CI_REGISTRY=$CI_REGISTRY \
+    --env CI_REGISTRY_USER=$CI_REGISTRY_USER \
+    --env CI_REGISTRY_PASSWORD=$CI_REGISTRY_PASSWORD \
