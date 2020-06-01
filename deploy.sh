bdcf26fb1f5a0d250a7196853f1c8096efb2893d
mpraski <marcin.praski@live.com>
2020-06-01 16:46:16 +0200

    Set correct vars

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,5 @@ source .gitlab-ci.env && gitlab-runner exec shell deploy \
     --env DO_KEY=$DO_KEY \
     --env DO_USER=$DO_USER \
     --env DO_HOST=$DO_HOST \
-    --env CI_REGISTRY_IMAGE=$CI_REGISTRY_IMAGE
+    --env CI_REGISTRY_CHATBOT=$CI_REGISTRY_CHATBOT \
+    --env CI_REGISTRY_STATIC=$CI_REGISTRY_STATIC
