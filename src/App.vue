b87a806a16fa53cefbee0498f733a450056cdac5
mpraski <marcin.praski@live.com>
2020-10-13 11:21:57 +0200

    Revert "Enable typescript"
    This reverts commit 9e4a3c3a86d460a7549ba45e8b4f334193af85d2.

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+<script>
+import HelloWorld from './components/HelloWorld.vue'
 
-@Options({
+export default {
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
+    HelloWorld
+  }
+}
 </script>
 
 <style>
